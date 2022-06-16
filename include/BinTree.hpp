17b99541768c200c5d527562032ed80bc8fbe9b9
AndreLiberato <andre.liberato@outlook.com.br>
2022-06-16 13:01:32 -0300

    Implementação do from_prefix que não era pra ter sido feita

--- include/BinTree.hpp
+++ include/BinTree.hpp
@@ -8,8 +8,8 @@ namespace bt {
     private:
       struct Node{
         std::string data; //!< The information we want to store in the node.
-        Node * left;      //!< A pointer to the left node in the list.
-        Node * right;     //!< A pointer to the right node in the list.
+        Node* left;      //!< A pointer to the left node in the list.
+        Node* right;     //!< A pointer to the right node in the list.
 
         //! Default Ctro.
         Node(const char &d=char{}, Node * l=nullptr, Node * r=nullptr)
@@ -17,7 +17,7 @@ namespace bt {
         { /*empty*/ }
       };
 
-      Node * root;  //!< The root node.
+      Node* root;  //!< The root node.
 
     public:
       // @brief The class constructor.
@@ -65,28 +65,83 @@ namespace bt {
       }
 
       /*
-       * @brief Gets a string in posfix notation and converts it to a binary tree.
-       * @param posfix: a string containing the posfix notation.
+       * @brief Gets a string and checking if the last character is an operator.
+       * @param postfix: a string containing the postfix notation.
+       * @return True if last character is a operator, false otherwise.
        */
-      std::string from_posfix(Node * node, std::string posfix) {
-        //cout << posfix << " -- " << posfix.back() << endl;
-        if(!posfix.empty()) {
-          if(posfix.back() == '+' || posfix.back() == '-' || posfix.back() == '*' || posfix.back() == '/') {
+      bool is_operator(char str) {
+        //std::cout << "Verificação:" << str << std::endl;
+        return (
+          ( str == '+' ||
+            str == '-' ||
+            str == '*' ||
+            str == '/' ||
+            str == '^' ) ? true : false);
+      }
+
+      /*
+       * @brief Gets a string in postfix notation and converts it to a binary tree.
+       * @param postfix: a string containing the postfix notation.
+       */
+      std::string from_postfix(Node* node, std::string postfix) {
+        if( !postfix.empty() ) {
+          if( is_operator(postfix.back()) ) {
+            /* 
+             * Note: To build a binary tree using postfix notation, 
+             * use preorder traversal on right first
+             */
+
             node->right = new Node();
             node->left = new Node();
-
-            node->data = posfix.back();
-
-            posfix.pop_back();
-            posfix = from_posfix(node->left, posfix);
-            posfix = from_posfix(node->right, posfix);
-            return posfix;
+            
+            node->data = postfix.back();
+      
+            postfix.pop_back();
+            postfix = from_postfix(node->right, postfix);
+            postfix = from_postfix(node->left, postfix);
+            return postfix;
           }
 
           else {
-            node->data = posfix.back();
-            posfix.pop_back();
-            return posfix;
+            node->data = postfix.back();
+            postfix.pop_back();
+            return postfix;
+          }
+        }
+        return "";
+      }
+
+      /*
+       * @brief Gets a string in prefix notation and converts it to a binary tree.
+       * @param prefix: a string containing the prefix notation.
+       */
+      std::string from_prefix(Node* node, std::string prefix) {
+        if( !prefix.empty() ) {
+          if( is_operator( prefix.front() )) {
+            /* 
+             * Note: To build a binary tree using prefix notation, 
+             * use preorder traversal on right first
+             */
+
+            node->left = new Node();
+            node->right = new Node();
+
+            node->data = prefix.front();
+
+            prefix.erase(prefix.begin());
+
+            prefix = from_prefix(node->left, prefix);
+            prefix = from_prefix(node->right, prefix);
+
+            return prefix;
+          }
+
+          else {
+            node->data = prefix.front();
+
+            prefix.erase(prefix.begin());
+
+            return prefix;
           }
         }
         return "";
@@ -94,49 +149,67 @@ namespace bt {
 
       /* 
        * @TODO Ver se a implementação está correta. Tem no slide!
-       * @brief Gets a string containing the posfix notation for the tree.
-       * @param posfix: just a variable to write the posfix notation in.
+       * @brief Gets a string containing the postfix notation for the tree.
+       * @param postfix: just a variable to write the postfix notation in.
        * @param node an Node containing the node that the tree is going to start to be converted. 
-       * @return A string containing the posfix notation.
+       * @return A string containing the postfix notation.
        */
-      std::string to_posfix(Node * node, std::string posfix="") {
-        if(node->right != nullptr)
-          posfix += to_posfix(node->right);
-        if(node->left != nullptr)
-          posfix += to_posfix(node->left);
-        posfix += visit(node);
-        return posfix;
+      std::string to_postfix(Node* node, std::string postfix="") {
+        //In postfix, first access the subtree on the left
+        if (node->left != nullptr)
+          postfix += to_postfix(node->left);
+        
+        //then on the right
+        if (node->right != nullptr)
+          postfix += to_postfix(node->right);
+
+        //and then visit the node.
+        postfix += visit(node);
+        
+        return postfix;
       }
 
       /* 
        * @brief Gives the prefix representation of the tree.
        * @return An string containing the tree's representation in prefix notation.
        */
-      std::string to_prefix() {
-        return "";
+      std::string to_prefix(Node * node, std::string prefix="") {
+        prefix += visit(node);
+
+        if (node->left != nullptr)
+          prefix += to_prefix(node->left);
+
+        if (node->right != nullptr)
+          prefix += to_prefix(node->right);
+
+        return prefix;
       }
 
       /* 
        * @brief Gives the infix representation of the tree.
        * @return An string containing the tree's representation in infix notation. 
        */
-      std::string to_infix(Node * node, std::string posfix="") {
+      std::string to_infix(Node * node, std::string infix="") {
         // Checks if the node is a leaf or root.
-        posfix += 
-          (((node->left == nullptr && node->right == nullptr) || node == root) ? "" : "(");
+        infix += (
+          ((node->left == nullptr && node->right == nullptr) 
+            || node == root) ? "" : "(" 
+          );
 
         if(node->left != nullptr)
-          posfix += to_infix(node->left);
+          infix += to_infix(node->left);
 
-        posfix += visit(node);
+        infix += visit(node);
 
         if(node->right != nullptr)
-          posfix += to_infix(node->right);
+          infix += to_infix(node->right);
 
         // Checks if the node is a leaf or root.
-        posfix += 
-          (((node->left == nullptr && node->right == nullptr) || node == root) ? "" : ")");
-        return posfix;
+        infix += (
+          ((node->left == nullptr && node->right == nullptr) 
+            || node == root) ? "" : ")"
+          );
+        return infix;
       }
 
       /* 
--- src/main.cpp
+++ src/main.cpp
@@ -4,21 +4,33 @@ using std::endl;
 
 #include "BinTree.hpp"
 using bt::BinTree;
-
-int main(int argc, char *argv[]) {
+//int argc, char *argv[]
+int main() {
 
   BinTree * binTree = new BinTree();
-  binTree->from_posfix(binTree->getRoot(), "ab*c+");
+
+  binTree->from_postfix(binTree->getRoot(), "ab*c+");
   binTree->print(binTree->getRoot());
-  cout << binTree->to_posfix(binTree->getRoot()) << endl;
+  cout << binTree->to_postfix(binTree->getRoot()) << endl;
   cout << binTree->to_infix(binTree->getRoot()) << endl;
   delete binTree;
 
+  std::cout << "_________________________________" << std::endl;
+
   //binTree->remove_node(binTree->getRoot());
   binTree = new BinTree();
-  binTree->from_posfix(binTree->getRoot(), "abc*+");
+  binTree->from_postfix(binTree->getRoot(), "abc*+");
   binTree->print(binTree->getRoot());
-  cout << binTree->to_posfix(binTree->getRoot()) << endl;
+  cout << binTree->to_postfix(binTree->getRoot()) << endl;
+  cout << binTree->to_infix(binTree->getRoot()) << endl;
+  delete binTree;
+
+  std::cout << "_________________________________" << std::endl;
+
+  binTree = new BinTree();
+  binTree->from_prefix(binTree->getRoot(), "+-*^2234//12+11");
+  binTree->print(binTree->getRoot());
+  cout << binTree->to_prefix(binTree->getRoot()) << endl;
   cout << binTree->to_infix(binTree->getRoot()) << endl;
 
   delete binTree;
